d7edbd9cd3786f7c9d79f32de2780809bb1b5446
brianmsj <brianmsj@gmail.com>
2017-07-23 20:10:29 -0700

    put in text areas

--- client/src/components/programinfo.js
+++ client/src/components/programinfo.js
@@ -11,12 +11,12 @@ class ProgramInfo extends React.Component {
 
         return (
         <div className='program-info row'>
-          <div className='col-5'>
+          <div className='program-info-container col-5'>
            <p className='program-text'>In 2012, only 2,000 students graduated from coding bootcamps. In 2017, over 20,000 student graduated from coding bootcamps.
            The competition is stronger than ever and face it, bootcamps are better at teaching students coding rather than finding a job.
            </p>
           </div>
-          <div className='col-5'>
+          <div className='program-info-container col-5'>
            <p className='program-text'>Our job search methods are proven, with some of our previous students landing developer jobs within 6 weeks of joining our program.
               We believe in our methods so strongly, that if you do not get a job within 5 months of working with our mentors, we will refund your money.
               To read more about the program click here.
